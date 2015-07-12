696f04025fb7301faf476cc0778b5c53e16608d2
Goppinath Thurairajah <goppinath.t@gmail.com>
2015-07-12 10:13:05 +0200

    Initial Commit

---
+++ GCDTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDTimer
+//
+//  Created by Goppinath Thurairajah on 12.07.15.
+//  Copyright (c) 2015 Goppinath Thurairajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCDTimer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GCDTimer
+//
+//  Created by Goppinath Thurairajah on 12.07.15.
+//  Copyright (c) 2015 Goppinath Thurairajah. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
